3397fb484f23a636d5f9cfd02ec7c2193609a559
tianhao <lsadth@qq.com>
2023-12-09 10:47:49 +0800

    测试

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,6 @@ export const SITE = {
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
   homePage: 'https://www.neijuan.fun',
-  blogPage: 'https://neijuan.fun/',
-  twitterId: '',
   githubId: 'zhonghaibin',
   repo: 'zhonghaibin/weekly'
 };
